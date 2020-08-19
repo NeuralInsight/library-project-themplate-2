8149a2d72fc925197ab733bb5b2980aee5163e2a
A.R.F.R <arfr@a.r.f.r>
2020-08-19 05:15:03 +0430

    fix book-content

---
+++ assets/js/book-content/book-content.js
@@ -0,0 +1,22 @@
+// $(".regular").slick({
+//     dots: true,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     responsive: [
+//         {
+//             breakpoint: 9999,
+//             settings: "unslick"
+//         },
+//         {
+//             breakpoint: 550,
+//              settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     dots: false,
+//                     autoplay: false,
+//                     }
+//         }
+//       ]
+// });
+  
