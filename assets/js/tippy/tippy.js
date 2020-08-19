fdce2c73a359ff9597e0574a26dee8e5403aa997
Ali <mojtabavi99@yahoo.com>
2020-08-19 17:01:00 +0430

    add tipy library to project

---
+++ assets/js/tippy/tippy.js
@@ -0,0 +1,18 @@
+tippy('#profile-icon', {
+    animation: 'scale',
+    placement: 'bottom',
+    content: 'Profile',
+})
+
+
+tippy('#settings-icon',{
+    animation: 'scale',
+    placement: 'bottom',
+    content: 'Profile Settings',
+})
+
+tippy('#notify-icon', {
+    animation: 'scale',
+    placement: 'bottom',
+    content: 'Your Notifications',
+});
